47eab02efb52a145e253c604cf901f75ead62259
KPpaolocaldera <paolo.caldera@keypartner.com>
2024-09-20 16:50:26 +0200

    feat: implement selectById and updateRanking methods
    Inside the service layer class, add the methods selectById and updateRanking, which updates the current ranking to a new value.
    
    References: #5

--- src/main/kotlin/com/keypartner/crud/service/TennisPlayerService.kt
+++ src/main/kotlin/com/keypartner/crud/service/TennisPlayerService.kt
@@ -8,9 +8,16 @@ import org.springframework.stereotype.Service
 @Service
 class TennisPlayerService(@Autowired val repository: TennisPlayerRepository) {
 
-    fun getAll(): List<TennisPlayer> = repository.findAll()
+    fun selectAll(): List<TennisPlayer> = repository.findAll()
+
+    fun selectById(id: Int): TennisPlayer = repository.findById(id).get()
 
     fun insert(player: TennisPlayer) = repository.save(player)
+
+    fun updateRanking(id: Int, ranking: Int) {
+        val player = repository.findById(id).get()
+        repository.save(player.copy(currentRanking = ranking))
+    }
 
     fun delete(id: Int) = repository.deleteById(id)
 }
