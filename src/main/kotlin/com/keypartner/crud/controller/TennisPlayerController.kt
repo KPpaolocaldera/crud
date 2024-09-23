0c21b28dc6f9ebb3e1594f82a050c105060fbbb3
KPpaolocaldera <paolo.caldera@keypartner.com>
2024-09-23 11:44:47 +0200

    feat: define PATCH and DELETE methods in the api
    Implement the definition of the patch method, which updates the ranking of the player, and the delete method, which deletes one player with given id.
    In this way, the implementation of CRUD operations is complete.
    
    References: #7

--- src/main/kotlin/com/keypartner/crud/controller/TennisPlayerController.kt
+++ src/main/kotlin/com/keypartner/crud/controller/TennisPlayerController.kt
@@ -3,16 +3,22 @@ package com.keypartner.crud.controller
 import com.keypartner.crud.model.TennisPlayer
 import com.keypartner.crud.service.TennisPlayerService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/kp/crud/tennisplayers")
 class TennisPlayerController(@Autowired private val service: TennisPlayerService) {
+
+    @PostMapping
+    fun createTennisPlayer(@RequestBody player: TennisPlayer): Int = service.insert(player)
 
     @GetMapping
     fun getTennisPlayers(): List<TennisPlayer> = service.selectAll()
@@ -20,6 +26,10 @@ class TennisPlayerController(@Autowired private val service: TennisPlayerService
     @GetMapping("/{id}")
     fun getTennisPlayerById(@PathVariable id: Int) = service.selectById(id)
 
-    @PostMapping
-    fun createTennisPlayer(@RequestBody player: TennisPlayer): Int = service.insert(player)
+    @PatchMapping("/{id}")
+    fun updateTennisPlayerRanking(@PathVariable id: Int, @RequestParam ranking: Int) =
+        service.updateRanking(id, ranking)
+    
+    @DeleteMapping("/{id}")
+    fun deleteTennisPlayer(@PathVariable id: Int) = service.delete(id)
 }
