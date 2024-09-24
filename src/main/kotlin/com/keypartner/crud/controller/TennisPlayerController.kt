506caff5f2973fe45633e2fbf61f9a1c07bd8c89
KPpaolocaldera <paolo.caldera@keypartner.com>
2024-09-24 12:27:53 +0200

    docs: let the swagger ui be available for the REST service implemented
    Add the org.springdoc webmvc dependency in the build.gradle.kts file for importing the necessary dependency to let the swagger ui be available via http call.
    Change the context path to /kp/crud in the application.properties file, so that all the REST endpoints share such path, which is going to be not specific anymore.
    
    References: #9

--- src/main/kotlin/com/keypartner/crud/controller/TennisPlayerController.kt
+++ src/main/kotlin/com/keypartner/crud/controller/TennisPlayerController.kt
@@ -3,9 +3,11 @@ package com.keypartner.crud.controller
 import com.keypartner.crud.model.Identifier
 import com.keypartner.crud.model.TennisPlayer
 import com.keypartner.crud.service.TennisPlayerService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -20,7 +22,7 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/kp/crud/tennisplayers")
+@RequestMapping("/tennisplayers")
 class TennisPlayerController(@Autowired private val service: TennisPlayerService) {
 
     @PostMapping
